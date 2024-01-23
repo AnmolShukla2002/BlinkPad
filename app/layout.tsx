de4af776e74d0a5f611108c68aa9e7dbdeb5400c
Anmol Shukla <anmol.28422@gmail.com>
2024-01-23 22:39:18 +0530

    few changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,10 +28,13 @@ export default function RootLayout({
       >
         <body>
           <ThemeProvider>
-            <div className="flex min-h-screen w-full flex-col items-center">
+            <div className="flex min-h-screen w-full flex-col items-center dark:bg-black">
               <NavBar />
               <Separator />
-              {children}
+              <main className="flex flex-grow w-full justify-center items-center dark:bg-neutral-950">
+                {" "}
+                {children}
+              </main>
             </div>
           </ThemeProvider>
         </body>
